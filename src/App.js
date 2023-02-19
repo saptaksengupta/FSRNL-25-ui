875af913a16a2451a282fd0be5cf6aec154c4186
saptaksengupta <deeps.sengupta@gmail.com>
2023-02-19 18:55:15 +0530

    feat: blog module implementation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import LoginPage from "./Pages/login/LoginPage";
+import PostEditorPage from "./Pages/PostEditor/PostEditorPage";
 import ProfilePage from "./Pages/Profile/Profile";
 import SignupPage from "./Pages/signup/SignupPage";
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/post-add" element={<PostEditorPage/>} />
         </Routes>
       </Router>
     </div>
